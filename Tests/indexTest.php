414e4bd8959d6acb6272a28887b7b535dd3f30a3
Francis Besset <francis.besset@gmail.com>
2011-12-20 17:05:35 +0100

    Added unit test

---
+++ Tests/indexTest.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * This file is part of the Best Framework Ever For Benchmark.
+ *
+ * (c) Alexandre Salomé <alexandre.salome@gmail.com>
+ */
+
+namespace BeSimple\SoapBundle\Tests\Soap;
+
+use BeSimple\SoapBundle\Soap\SoapRequest;
+
+/**
+ * UnitTest for index.html.
+ *
+ * @author Besset Francis <francis.besset@gmail.com>
+ */
+class indexTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIndex()
+    {
+        $this->assertEquals('Hello world!'."\n", $this->loadIndexFile());
+    }
+
+    private function loadIndexFile()
+    {
+        return file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'index.html');
+    }
+}
+
